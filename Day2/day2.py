c1291f5dc6a705a01323d967ae5865bf2ea06e10
Arttu Kuitunen <arsaku@utu.fi>
2024-05-15 12:33:52 +0300

    Day 2 puzzle solved, if statement logic was wrong in the sum_op_ids() function. Not yet reviewed others

--- Day2/day2.py
+++ Day2/day2.py
@@ -5,7 +5,7 @@
 def parse_input_line(line):
     game_id, string = line.split(":")
     game_id = int(game_id.strip().split()[1]) 
-    print("GameID: ", game_id)
+    #print("GameID: ", game_id)
 
     throws = string.strip().split(";") # Splits the string
     
@@ -15,43 +15,59 @@ def parse_input_line(line):
     for throw in throws:
         # Split throw by comma and remove leading/trailing whitespace
         colors = throw.strip().split(",")
-        print("Colors by throw after ',' split:", colors)
+        #print("Colors by throw after ',' split:", colors)
         # Iterate over color-value pairs
         for color_value in colors:
             value, color = color_value.strip().split()
-            print("Trying to split color/value pair.", "color:", color, "value:", value)
+            #print("Trying to split color/value pair.", "color:", color, "value:", value)
             color_counts[f"{color}"].append(int(value))
-            print("color_counts:", color_counts)
+            #print("color_counts:", color_counts)
 
     return color_counts
 
-# Opening the input file
-try:
+# Converting all input data to better format -> returning a JSON dict
+def save_data_to_json():
+    try:
+        games = {"games": []}
+
+        with open("Day2/input-day2.txt", "r") as file:
+            for line in file:
+                
+                game_data = parse_input_line(line)
+                game_id = game_data["id"]
+                red_throws = game_data.get("red", [])
+                green_throws = game_data.get("green", [])
+                blue_throws = game_data.get("blue", [])
+                game_info = {"id": game_id, "red": red_throws, "green": green_throws, "blue": blue_throws}
+                games["games"].append(game_info)
+
+            #print(games)
+        return games
+    except Exception as e:
+        print(type(e))    # the exception type
+        print(e.args)     # arguments stored in .args
+        print(e)        
+
+def sum_of_ids():
     red_limit = 12
     green_limit = 13
     blue_limit = 14
+    games = save_data_to_json()
+    sum_of_ids = 0
 
-    games = {"games": []}
+    for data_list in games["games"]:
+        # print("Game ID:", data_list["id"])
+        # print("Red throws:", data_list["red"])
+        # print("Green throws:", data_list["green"])
+        # print("Blue throws:", data_list["blue"])
+        print("Red max:", max(data_list["red"]))
+        print("Green max:", max(data_list["green"]))
+        print("Blue max:", max(data_list["blue"]))
 
-    with open("Day2/input-day2.txt", "r") as file:
-        for line in file:
-            
-            game_data = parse_input_line(line)
+        if red_limit >= max(data_list["red"]) and green_limit >= max(data_list["green"]) and blue_limit >= max(data_list["blue"]):
+            sum_of_ids += data_list["id"]
+    return sum_of_ids
 
-            print("Trying to save processed line into games: ")
-
-            game_id = game_data["id"]
-            red_throws = game_data.get("red", [])
-            green_throws = game_data.get("green", [])
-            blue_throws = game_data.get("blue", [])
-            game_info = {"id": game_id, "red": red_throws, "green": green_throws, "blue": blue_throws}
-            games["games"].append(game_info)
-
-        print(games)
-except Exception as e:
-    print(type(e))    # the exception type
-    print(e.args)     # arguments stored in .args
-    print(e)        
-    
-
-
+result = sum_of_ids()
+print("Result for the advent of code 2023 day2 is:")
+print(result)
